95cb601cd18dae2206f0002035fdc1522eb98941
Alvaro-LGPA <alvarolaura@gmail.com>
2023-08-08 23:12:32 +0200

    add base subfolder definition

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,14 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/reddit-client/',
   server: {
     open: true,
   },
   build: {
     outDir: "build",
-    sourcemap: true,
+    sourcemap: true
+    
   },
   test: {
     globals: true,
